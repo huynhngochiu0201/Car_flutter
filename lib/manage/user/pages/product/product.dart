80b723dc79a33b0e843070dff35545f11e3c46e0
Huỳnh Hiếu <huynhhieu@MacBook-Pro-cua-Huynh.local>
2024-05-24 00:02:21 +0700

    fix product

--- lib/manage/user/pages/product/product.dart
+++ lib/manage/user/pages/product/product.dart
@@ -1,9 +1,9 @@
-import 'package:car_flutter/manage/user/models/product_model.dart';
 import 'package:car_flutter/manage/user/models/user_model.dart';
-import 'package:flutter/cupertino.dart';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../models/product_model.dart';
 
 class ProductCart extends StatefulWidget {
   const ProductCart({super.key, required this.user});
@@ -17,79 +17,86 @@ class _ProductCartState extends State<ProductCart> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0XFFF8F9FA),
       body: Stack(
         children: [
-          SingleChildScrollView(
+          Positioned(
+            top: 0.0,
+            right: 0.0,
+            left: 0.0,
+            child: Image.asset(
+              widget.user.background ?? '',
+              width: double.infinity,
+              fit: BoxFit.cover,
+              height: 168.0,
+            ),
+          ),
+          Positioned(
+            top: MediaQuery.of(context).padding.top + 10.0,
+            left: 22.0,
+            child: GestureDetector(
+              onTap: () {
+                Navigator.pop(context);
+              },
+              child: CircleAvatar(
+                backgroundColor: Colors.white,
+                child: SvgPicture.asset(
+                  'assets/icons/ic_back.svg',
+                  color: Colors.black,
+                ),
+              ),
+            ),
+          ),
+          Positioned.fill(
+            top: 148,
             child: Column(
               children: [
-                const CircleAvatar(
-                  child: Icon(Icons.abc),
-                ),
-                Image.asset(
-                  height: 168.0,
-                  widget.user.background ?? '',
-                  width: double.infinity,
-                  fit: BoxFit.cover,
-                ),
-                const SizedBox(height: 20.0),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                  child: Column(
-                    children: [
-                      GestureDetector(
-                        onTap: () {},
-                        child: Container(
-                          decoration: const BoxDecoration(
-                              borderRadius:
-                                  BorderRadius.all(Radius.circular(10.0)),
-                              color: Colors.white),
-                          height: 100.0,
-                          width: double.infinity,
-                          child: const Text('Hello'),
-                        ),
+                Expanded(
+                  child: Container(
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20),
+                        topRight: Radius.circular(20),
                       ),
-                      GridView.builder(
-                        physics: const NeverScrollableScrollPhysics(),
-                        shrinkWrap: true,
-                        gridDelegate:
-                            const SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 2,
-                          crossAxisSpacing: 20.0,
-                          mainAxisSpacing: 20.0,
-                          mainAxisExtent: 220,
-                        ),
-                        itemBuilder: (_, index) {
-                          final productItem = product[index];
-                          return Container(
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(
-                                16.0,
-                              ),
-                              color: Colors.white,
+                    ),
+                    child: GridView.builder(
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 20.0,
+                        mainAxisSpacing: 20.0,
+                        mainAxisExtent: 220,
+                      ),
+                      itemBuilder: (_, index) {
+                        final productItem = product[index];
+                        return Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(
+                              16.0,
                             ),
-                            child: Column(
-                              children: [
-                                Padding(
-                                  padding: const EdgeInsets.all(10.0),
-                                  child: ClipRRect(
-                                    borderRadius: BorderRadius.circular(10.0),
-                                    child: Image.asset(
-                                      productItem.image ?? '',
-                                      height: 150,
-                                      width: double.infinity,
-                                      fit: BoxFit.cover,
-                                    ),
+                            color: Colors.black,
+                          ),
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(10.0),
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                  child: Image.asset(
+                                    productItem.image ?? '',
+                                    height: 150,
+                                    width: double.infinity,
+                                    fit: BoxFit.cover,
                                   ),
                                 ),
-                                Text(productItem.title ?? '')
-                              ],
-                            ),
-                          );
-                        },
-                        itemCount: product.length,
-                      ),
-                    ],
+                              ),
+                              Text(productItem.title ?? '')
+                            ],
+                          ),
+                        );
+                      },
+                      itemCount: product.length,
+                    ),
                   ),
                 ),
               ],
